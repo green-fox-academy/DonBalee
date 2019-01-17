6c6467293b443aed7acd037d0dd458e707d0d15e
DonBalee <veghbali81@gmail.com>
2019-01-17 17:29:05 +0100

    strings

---
+++ Week-01/Day-04/Strings.ts
@@ -0,0 +1,5 @@
+console.log('apple');
+console.log('don\'t');
+console.log('"Everything you can imagine is real." - Picasso');
+console.log('tooth' + 'brush');
+console.log('My favorite number is: ' + 8);
