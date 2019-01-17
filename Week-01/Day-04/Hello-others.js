e57bc88b0bb71e5bc7ce68d378e80792ae1cb2e6
DonBalee <veghbali81@gmail.com>
2019-01-17 16:25:57 +0100

    Done with exercise

---
+++ Week-01/Day-04/Hello-others.js
@@ -0,0 +1,4 @@
+'use strict';
+console.log('Hello, Lili!');
+console.log('Hello, Leo!');
+console.log('hello, Ádám!');
---
+++ Week-01/Day-04/Hello-others.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+console.log('Hello, Lili!');
+console.log('Hello, Leo!');
+console.log('hello, Ádám!');
