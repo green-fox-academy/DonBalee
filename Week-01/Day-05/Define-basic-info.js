6a446ca4eb929eb0f38e553cb03cbf854e6a65ca
DonBalee <veghbali81@gmail.com>
2019-01-18 15:32:54 +0100

    basic info

---
+++ Week-01/Day-05/Define-basic-info.js
@@ -0,0 +1,2 @@
+var Mynameis = 'Balázs Végh';
+console.log('My name is Balázs Végh');
---
+++ Week-01/Day-05/Define-basic-info.ts
@@ -0,0 +1,8 @@
+let Mynameis: string = 'Balázs Végh';
+console.log('My name is Balázs Végh');
+let Iam: number = 37;
+console.log('I am ' + 37);
+let height: number = 177;
+console.log('I am ' + 177)
+let Maritalstatus: boolean = false;
+console.log(Maritalstatus);
