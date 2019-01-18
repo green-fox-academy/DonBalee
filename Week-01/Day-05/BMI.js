c949c6d378d77848b1d0c9c7e8cb36ab0be6b5a8
DonBalee <veghbali81@gmail.com>
2019-01-18 14:45:38 +0100

    BMI

---
+++ Week-01/Day-05/BMI.js
@@ -0,0 +1,4 @@
+'use strict';
+var massInKg = 81.2;
+var heightInM = 1.78;
+console.log('massInKg' / 'heightInM');
---
+++ Week-01/Day-05/BMI.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+console.log(81.2 / 1.78);
+
