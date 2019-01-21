8704663fd1e3cb66a468c78e6b80a09ce319a06a
DonBalee <veghbali81@gmail.com>
2019-01-21 17:12:52 +0100

    sum

---
+++ Week-02/Day-01/sum.js
@@ -0,0 +1,8 @@
+var a = 0;
+var b = 1;
+var c = 3;
+function sum(d, e, f) {
+    var result = d + e + f;
+    console.log(result);
+}
+sum(a, b, c);
---
+++ Week-02/Day-01/sum.ts
@@ -0,0 +1,11 @@
+let a : number = 0;
+let b : number = 1;
+let c : number = 3;
+
+function sum(d, e, f) {
+let result : number = d + e + f;
+console.log(result);
+
+}
+
+sum(a, b ,c);
