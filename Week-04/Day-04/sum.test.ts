1d1e3b2bfcce315af388a459da99a4da06c0476f
DonBalee <veghbali81@gmail.com>
2019-02-07 15:16:53 +0100

    rewritten_code_for_sums

---
+++ Week-04/Day-04/sum.test.ts
@@ -0,0 +1,19 @@
+import {test} from 'tape';
+import {Numbers} from './sum';
+
+test('add 2 numbers', t => {
+    const operations = new Numbers();
+    let actual = operations.sum(3, 5);
+    let expected = 8;
+    t.equal(8, 8);
+    t.end();
+});
+
+test('empty list', t => {
+    const operations1 = new Numbers();
+    let actual = operations1.sum();
+    let expected = 8;
+    t.equal();
+    t.end();
+
+})
--- Week-04/Day-04/sum.ts
+++ Week-04/Day-04/sum.ts
@@ -1,10 +1,11 @@
 'use strict'
 export class Numbers{
-sum(num1: number, num2: number): number {
-    
-    return num1 + num2;
+sum(list: number[]) {
+    let sum = 0
+    list.forEach((item) => {sum += item} )
+    return sum; 
 }
 }
 
 const Numbers1 = new Numbers();
-console.log(Numbers1.sum(3,5));
+console.log(Numbers1.sum([1,2,3]));
